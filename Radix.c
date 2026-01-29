948f0dd950dc27a655bbd9cf66e3b25fe7c9e563
gibgibbb <diazkristian321@gmail.com>
2026-01-29 21:14:31 +0800

    Insert First Recap

--- Radix.c
+++ Radix.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct {
+typedef struct node{
     int data;
     struct node *next;
-}node, *nodePtr;
+}*nodePtr;
 
-void init(node *A){
-    A->next = NULL;
+void init(nodePtr *A){
+    *A = NULL;
+}
+
+void insertFirst(nodePtr *A, int x){
+	nodePtr temp = malloc(sizeof(struct node));
+	if(temp != NULL){
+		temp->data = x;
+		temp->next = *A;
+		*A = temp;
+	}
+}
+
+void displayList(nodePtr A){
+	for(; A != NULL; A = A->next){
+		printf("%d ", A->data);
+	}
 }
 
 int main() {
 
     nodePtr main;
-    init(main);
+    init(&main);
+    insertFirst(&main, 10);
+    insertFirst(&main, 5);
+    insertFirst(&main, 500);
+    insertFirst(&main, 39);
+    insertFirst(&main, 259);
+    
+    displayList(main);
     
     return 0;
 }
