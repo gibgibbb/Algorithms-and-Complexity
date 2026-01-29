72c7b0bec5649d76d8d4f9fddac50e1a6fb90b1c
gibgibbb <diazkristian321@gmail.com>
2026-01-29 21:52:20 +0800

    Full radixSort with helper function

--- Radix.c
+++ Radix.c
@@ -19,10 +19,64 @@ void insertFirst(nodePtr *A, int x){
 	}
 }
 
+void insertLast(nodePtr *A, nodePtr temp){
+    if(*A == NULL){
+        temp->next = NULL;
+        *A = temp;
+    } else {
+        nodePtr trav;
+        for(trav = *A; trav->next != NULL; trav = trav->next){}
+        temp->next = NULL;
+        trav->next = temp;
+    }
+}
+
 void displayList(nodePtr A){
 	for(; A != NULL; A = A->next){
 		printf("%d ", A->data);
 	}
+}
+
+int getMax(nodePtr A){
+    int max = 0;
+    for(; A != NULL; A = A->next){
+        if(A->data > max){
+            max = A->data;
+        }
+    }
+    return max;
+}
+
+void radixSort(nodePtr *A){
+    int max = getMax(*A);
+
+    for(int exp = 1; max / exp > 0; exp *= 10){
+
+        nodePtr bucket[10];
+        for(int i = 0; i < 10; i++){
+            bucket[i] = NULL;
+        }
+
+        nodePtr trav = *A;
+        for(; trav != NULL; ){
+            nodePtr temp = trav;
+            trav = trav->next;
+
+            int digit = (temp->data / exp) % 10;
+            insertLast(&bucket[digit], temp);
+        }
+
+        *A = NULL;
+        nodePtr *tail = A;
+
+        for(int i = 0; i < 10; i++){
+            if(bucket[i] != NULL){
+                *tail = bucket[i];
+                for(; (*tail)->next != NULL; tail = &(*tail)->next){}
+                tail = &(*tail)->next;
+            }
+        }
+    }
 }
 
 int main() {
@@ -35,6 +89,7 @@ int main() {
     insertFirst(&main, 39);
     insertFirst(&main, 259);
     
+    radixSort(&main);
     displayList(main);
     
     return 0;
